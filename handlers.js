6284e44dce614cc8789c7c4bce92e466255b02af
jkuusist <jonikuusisto12@gmail.com>
2021-12-23 13:42:11 +0200

    added error handling for bearTrend when the status is not OK or when the content type is wrong

--- handlers.js
+++ handlers.js
@@ -1,10 +1,27 @@
 const https = require('https')
 
 function handleBearTrend(req, res) {
+	let startDate = req.headers
+
 //	https.get('https://httpbin.org/get', response => {
 	https.get('https://api.coingecko.com/api/v3/ping', response => {
 		const { statusCode } = response
 		const contentType = response.headers['content-type']
+
+		console.log('contentType: ', contentType)
+		console.log('typeof contentType: ', typeof(contentType))
+
+		let error
+
+		if (statusCode != 200) {
+			error = new Error(`Request failed. Status code: ${statusCode}`)
+		} else if (!contentType.includes('application/json')) {
+			error = new Error(`Invalid content type: ${contentType}`)
+		}
+
+		if (error) {
+			console.error(error.message)
+		}
 
 		response.setEncoding('utf8')
 		let rawData = ''
