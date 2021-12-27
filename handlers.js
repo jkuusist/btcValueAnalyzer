6f8ce808cac7ca9900f45ab63eaa0bba07b425bf
jkuusist <jonikuusisto12@gmail.com>
2021-12-27 10:28:31 +0200

    added error handling for when no start date or no end date is provided

--- handlers.js
+++ handlers.js
@@ -7,43 +7,51 @@ function handleBearTrend(req, res) {
 	let unixStartDate = startDate.getTime() / 1000
 	let unixEndDate = endDate.getTime() / 1000 + 3600
 
-	https.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${unixStartDate}&to=${unixEndDate}`, response => {
-		const { statusCode } = response
-		const contentType = response.headers['content-type']
+	if (startDate.toString() === 'Invalid Date') {
+		res.send('Invalid start date')
+	} else if (endDate.toString() === 'Invalid Date') {
+		res.send('Invalid end date')
+	} else if (unixStartDate >= (unixEndDate - 3600)) {
+		res.send('End date must be later than start date')
+	} else {
+		https.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${unixStartDate}&to=${unixEndDate}`, response => {
+			const { statusCode } = response
+			const contentType = response.headers['content-type']
 
-		console.log('statusCode: ', statusCode)
+			let error
 
-		let error
-
-		if (statusCode != 200) {
-			error = new Error(`Request failed. Status Code: ${statusCode}`)
-		} else if (!contentType.includes('application/json')) {
-			error = new Error(`Invalid content type: ${contentType}`)
-		}
-
-		if (error) {
-			console.error(error.message)
-		}
-
-		response.setEncoding('utf8')
-		let rawData = ''
-
-		response.on('data', (chunk) => {
-			rawData += chunk
-		})
-
-		response.on('end', () => {
-			try {
-				const parsedData = JSON.parse(rawData)
-
-				console.log('parsedData: ', parsedData)
-
-				res.send(parsedData)
-			} catch (e) {
-				console.error(e.message)
+			if (statusCode != 200) {
+				error = new Error(`Request failed. Status Code: ${statusCode}`)
+			} else if (!contentType.includes('application/json')) {
+				error = new Error(`Invalid content type: ${contentType}`)
 			}
+
+			if (error) {
+				console.error(error.message)
+			}
+
+			response.setEncoding('utf8')
+			let rawData = ''
+
+			response.on('data', (chunk) => {
+				rawData += chunk
+			})
+
+			response.on('end', () => {
+				try {
+					const parsedData = JSON.parse(rawData)
+
+	//				console.log('parsedData: ', parsedData)
+
+					console.log(parsedData.prices)
+
+					res.send(parsedData)
+				} catch (e) {
+					console.error(e.message)
+				}
+			})
 		})
-	})
+	}
 }
 
 exports.handleBearTrend = handleBearTrend
